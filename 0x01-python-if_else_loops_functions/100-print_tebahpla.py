0eb16c5a558ec5e48377f988df7cb8606b443410
rekebuba <abukiabdullahi1@gmail.com>
2024-01-02 21:55:12 +0300

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print("{}".format(chr(i)), end= "")
+        print("{}".format(chr(i)), end="")
     else:
-        print("{}".format(chr(i - 32)), end = "")
+        print("{}".format(chr(i - 32)), end="")

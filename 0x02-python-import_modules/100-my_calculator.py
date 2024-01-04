fd269da3705975425e23665bd3c3f61dc96b63be
rekebuba <abukiabdullahi1@gmail.com>
2024-01-04 16:12:29 +0300

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 if __name__ == '__main__':
     from sys import argv
     from calculator_1 import add, sub, mul, div
+
     args = argv[1:]
     if len(args) != 3:
         print("usage: ./100-my_calculator.py <a> <operator> <b>")

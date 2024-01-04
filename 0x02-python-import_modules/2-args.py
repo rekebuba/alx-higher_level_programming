5730bdd89957be41a24620829fb00262da3ac2c8
rekebuba <abukiabdullahi1@gmail.com>
2024-01-04 15:15:23 +0300

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 arguments = sys.argv[1:]
 length = len(arguments)
 
-print(" : " if length == 0 else f"{length} argument{'s' if length != 1 else ''}:")
+print("0 arguments." if length == 0 else f"{length} argument{'s' if length != 1 else ''}:")
 a = 1
 for arg in arguments:
     print("{}: {}".format(a, arg))

bd5e89e794225d8908e4dae047b1f2b07efb03a1
rekebuba <abukiabdullahi1@gmail.com>
2024-01-05 23:03:34 +0300

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a liked list is a palindrome
+ * @head: a pointer to a linked list
+ * the approach i fallowed is that
+ * first create a copy the linked list
+ * second reverse the copy list
+ * third compare the two linked list
+ * Return: 0 if it is not, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr = *head;
+	listint_t *new_node = NULL;
+	listint_t *current = NULL;
+/* creating a new node */
+	while (ptr != NULL)
+	{
+		listint_t *node = malloc(sizeof(listint_t));
+
+		node->n = ptr->n;
+		node->next = NULL;
+		if (new_node == NULL)
+		{
+			new_node = node;
+			current = new_node;
+		}
+		else
+		{
+			current->next = node;
+			current = current->next;
+		}
+		ptr = ptr->next;
+	}
+
+	reverse_node(&new_node);
+
+	listint_t *ptr1 = *head;
+	listint_t *ptr2 = new_node;
+
+	while (ptr1 != NULL)
+	{
+		if (ptr1->n != ptr2->n)
+		{
+			free_listint(new_node);
+			return (0);
+		}
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next;
+	}
+	free_listint(new_node);
+	return (1);
+}
+
+/**
+ * reverse_node - reverse a node
+ * @head: the linked list need to be reversed
+ */
+void reverse_node(listint_t **head)
+{
+	listint_t *new_node = *head;
+	listint_t *temp1 = NULL;
+	listint_t *temp2;
+
+	while (new_node != NULL)
+	{
+		temp2 = new_node->next;
+		new_node->next = temp1;
+		temp1 = new_node;
+		new_node = temp2;
+	}
+	*head = temp1;
+}

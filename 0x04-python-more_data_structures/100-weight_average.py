e46a8a587cd935c6fc6e73e773a1d5400d1fea68
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 23:07:19 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return None
 
     total = 0
     last_values = 0

f520906aa718e2f6ddd0da7131e83c242068299a
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 17:41:00 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    for value in a_dictionary:
-        
+    if a_dictionary == None:
+        return None
+
+    max_score = float('-inf')
+    max_key = None
+
+    for key, value in a_dictionary.items():
+        if value > max_score:
+            max_score = value
+            max_key = key
+
+    return max_key

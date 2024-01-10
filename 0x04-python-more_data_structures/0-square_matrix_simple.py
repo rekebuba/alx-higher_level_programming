7402285eb503ec3db02ba3919a4f6dd28ff14333
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 20:56:00 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     if len(matrix) == 0:
         return None
 
-    square = lambda x : x ** 2
+    square = lambda x: x ** 2
     row = [[square(x) for x in y] for y in matrix]
 
     return row

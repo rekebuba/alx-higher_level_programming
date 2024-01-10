e9f8c32148d1142ffc4db603eb7896b3564e1ce2
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 23:19:13 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
 
-    total = 0
-    last_values = 0
+    arr = []
+    denom = 0
 
-    for i in my_list:
-        total += add_tuple(i)
+    for i, j in my_list:
+        arr.append(i * j)
+        denom += j
 
-    for j in my_list:
-        last_values += j[1]
-
-    average = total / last_values
-    return average
-
-
-def add_tuple(x):
-    return x[0] * x[1]
+    avg = sum(arr) / denom
+    return avg
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dictionary = {}
+    to_delete = []
     for key, val in a_dictionary.items():
-        if val != value:
-            new_dictionary[key] = val
-    return new_dictionary
+        if val == value:
+            to_delete.append(key)
+
+    for key in to_delete:
+        del a_dictionary[key]
+
+    return a_dictionary

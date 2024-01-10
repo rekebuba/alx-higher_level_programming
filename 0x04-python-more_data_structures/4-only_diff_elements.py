b15705b718887bc6e005999c1209d2efe3a86fd3
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 23:01:28 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     arr = []
     for element in set_1:
-        if element not in arr:
+        if element not in set_2:
             arr.append(element)
     for element in set_2:
-        if element not in arr:
+        if element not in set_1:
             arr.append(element)
     return arr

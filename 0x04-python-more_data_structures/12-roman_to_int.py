e64b5d4bca2182545095207229cee6b5a3cac0ab
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 19:11:22 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    
+    if type(roman_string) != str  or None:
+        return 0
+    dictionary = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prv = 0
+    for char in reversed(roman_string):
+        result = dictionary.get(char)
+        if result < prv:
+            total -= result
+        else:
+            total += result
+            prv = result
+    return total

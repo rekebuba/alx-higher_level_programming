3b38965d7362654de9475d990183c4674c5dd9ea
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 19:12:21 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str  or None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     dictionary = {
         'I': 1,

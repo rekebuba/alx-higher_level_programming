39471b42f4e80eb5994c807b9cfff4f09c315b3c
rekebuba <abukiabdullahi1@gmail.com>
2024-01-10 10:44:34 +0300

    0x03-python-data_structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,9 @@ def uniq_add(my_list=[]):
     if len(my_list) == 0:
         return None
     add = 0
+    arr = []
     for element in my_list:
-        if my_list.count(element) == 0:
+        if element not in arr:
             add += element
+            arr.append(element)
     return add

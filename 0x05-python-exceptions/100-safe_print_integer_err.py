326ad12f51bb87d2ac8d1aa6ad4e92c42dcac9af
rekebuba <abukiabdullahi1@gmail.com>
2024-01-22 22:06:08 +0300

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as ex:
+        sys.stderr.write("Exception: {}\n".format(str(ex)))
+        return False
+    else:
+        return True

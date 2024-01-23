0228939c7425f8e24a7d5fa0985dca7872c5f7e8
rekebuba <abukiabdullahi1@gmail.com>
2024-01-23 21:33:55 +0300

    0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" define a class Square """
+
+
+class Square:
+    """
+    it defines a square by: (based on 0-square.py)
+    """
+    def __init__(self, size):
+        """ private instantiation with size
+
+        Args:
+            size (_type_): the size to be squared
+        """
+        self.__size = size

c90822e4dbebbb5fea140c9e097a2e8454a7ed45
rekebuba <abukiabdullahi1@gmail.com>
2024-01-27 23:22:13 +0300

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -3,8 +3,8 @@ matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
     [1, 2, 3],
-    [4, 5, 6]
+    [4, 5, 6],
+    4
 ]
 print(matrix_divided(matrix, 3))
 print(matrix)
-
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    new_matrix = [[]]
+    new_matrix = []
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    for i, part in enumerate(matrix):
-        if len(part) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for j, item in enumerate(part):
-            if not isinstance(item, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            result = item / div
-            new_matrix.append(result)
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list \
+                of lists) of integers/floats")
+        if not all(isinstance(item, (int, float)) for item in row):
+            raise TypeError("matrix must be a matrix (list of \
+                lists) of integers/floats")
+
+    if any(len(row) != len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    for row in matrix:
+        result = [round(item / div, 2) for item in row]
+        new_matrix.append(result)
     return new_matrix

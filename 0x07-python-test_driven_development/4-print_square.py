810f7029787a7a9c6905e41fd007e5f7b1ba5451
rekebuba <abukiabdullahi1@gmail.com>
2024-01-28 12:05:05 +0300

    0x07-python-test_driven_development

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    if not isinstance(size, int) or (isinstance(size, float) and size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print('#' * size)

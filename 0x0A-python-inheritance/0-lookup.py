efea7bc24a5c4d023849df5c629bca572126c010
rekebuba <abukiabdullahi1@gmail.com>
2024-02-05 21:15:40 +0300

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+returns the list of available attributes and methods of an object
+"""
+def lookup(obj):
+    """ returns the list of available attributes
+        and methods of an object
+
+    Returns:
+        _type_: list of available
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+lookup = __import__('0-lookup').lookup
+
+class MyList(lookup):
+    
+    def print_sorted(self):
+        ...

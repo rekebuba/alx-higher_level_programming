70bb5c723f79a8af7ab9c83e4ff52adbe7a12b68
rekebuba <abukiabdullahi1@gmail.com>
2024-02-07 11:51:51 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,9 +5,6 @@ and then save them to a file:
 """
 
 import sys
-import json
-import os.path
-
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -15,7 +12,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 try:
-    items = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 except FileNotFoundError:
     my_list = []
 

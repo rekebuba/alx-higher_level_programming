aef5a015f71271f4ec295e4455acce800b4f1b96
rekebuba <abukiabdullahi1@gmail.com>
2024-02-07 22:46:56 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,7 +14,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 filename = "add_item.json"
-my_list = []
 try:
     items = load_from_json_file(filename)
 except FileNotFoundError:

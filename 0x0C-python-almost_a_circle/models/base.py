8e0f9c433ff84b20d0b830c151fa0eb5ea9fa18e
rekebuba <abukiabdullahi1@gmail.com>
2024-02-10 17:12:48 +0300

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Class Base """
 import json
+import os
 
 class Base:
     """
@@ -45,3 +46,25 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """  returns an instance with all attributes already set """
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ a Class method that returns a list of instances """
+        result = []
+        filename = cls.__name__ + ".json"
+        if os.path.exists(filename):
+            with open(filename, "r") as file:
+                my_lists = cls.from_json_string(file.read())
+                for my_list in my_lists:
+                    result.append(cls.create(**my_list))
+        return result
